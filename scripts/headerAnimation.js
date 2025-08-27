1cc8de94e489fff69245e12740e683eafedc9ee1
Finn-Oskar <finnoskar@mikkelsen.co.nz>
2025-08-27 17:39:04 +1200

    improved animation header

--- scripts/headerAnimation.js
+++ scripts/headerAnimation.js
@@ -2,14 +2,14 @@ document.addEventListener("scroll", () => {
     // Find elements and define constants
     let heading = document.getElementById("heading");
     let animatedHeaderTexts = document.querySelectorAll("#heading h1");
-    const HEADING_HEIGHT = heading.offsetHeight;
+    const HEADING_HEIGHT = heading.offsetHeight + 300;
     const DOCUMENT_WIDTH = 100; // in vw
     const NATURAL_LEFT = 40; // Natural left value of the headers, in vw;
 
 
     if (window.scrollY < HEADING_HEIGHT) {
         for (let headingText of animatedHeaderTexts) {
-            let scrolledRatio = window.scrollY / (0.7 * HEADING_HEIGHT);
+            let scrolledRatio = window.scrollY / (0.8 * HEADING_HEIGHT);
             if (["1", "3"].includes(headingText.getAttribute("data-header-anim-number"))) {
                 let changeInLeft = -1.5 * scrolledRatio * NATURAL_LEFT;
                 var newLeftPos = NATURAL_LEFT + changeInLeft;
