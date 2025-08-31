3017980d2b095f3841af8a899fa060688b1107b0
Finn-Oskar <finnoskar@mikkelsen.co.nz>
2025-09-01 09:45:16 +1200

    worked on image separator

--- scripts/headerAnimation.js
+++ scripts/headerAnimation.js
@@ -4,27 +4,25 @@ document.addEventListener("scroll", () => {
     let animatedHeaderTexts = document.querySelectorAll("#heading h1");
     const HEADING_HEIGHT = heading.offsetHeight + 300;
     const DOCUMENT_WIDTH = 100; // in vw
-    const NATURAL_LEFT = 40; // Natural left value of the headers, in vw;
+    const NATURAL_LEFT = 25; // Natural left value of the headers, in vw;
 
-
-    if (window.scrollY < HEADING_HEIGHT) {
-        for (let headingText of animatedHeaderTexts) {
-            let scrolledRatio = window.scrollY / (0.8 * HEADING_HEIGHT);
-            if (["1", "3"].includes(headingText.getAttribute("data-header-anim-number"))) {
-                let changeInLeft = -1.5 * scrolledRatio * NATURAL_LEFT;
-                var newLeftPos = NATURAL_LEFT + changeInLeft;
-                    if (window.scrollY > 0) {
-                    newLeftPos -= 5 * parseInt(headingText.getAttribute("data-header-anim-number")) * scrolledRatio;
-                }
-            } else {
-                var newLeftPos = (NATURAL_LEFT + (scrolledRatio * (DOCUMENT_WIDTH - NATURAL_LEFT)));
+    for (let headingText of animatedHeaderTexts) {
+        let scrolledRatio = window.scrollY / (0.7 * HEADING_HEIGHT);
+        if (["1", "3"].includes(headingText.getAttribute("data-header-anim-number"))) {
+            let changeInLeft = -1.5 * scrolledRatio * NATURAL_LEFT;
+            var newLeftPos = NATURAL_LEFT + changeInLeft;
                 if (window.scrollY > 0) {
-                    newLeftPos += 5 * parseInt(headingText.getAttribute("data-header-anim-number")) * scrolledRatio;
-                }
+                newLeftPos -= 5 * parseInt(headingText.getAttribute("data-header-anim-number")) * scrolledRatio;
             }
-            headingText.style.left = newLeftPos + "vw";
+        } else {
+            var newLeftPos = (NATURAL_LEFT + (scrolledRatio * (DOCUMENT_WIDTH - NATURAL_LEFT)));
+            if (window.scrollY > 0) {
+                newLeftPos += 5 * parseInt(headingText.getAttribute("data-header-anim-number")) * scrolledRatio;
+            }
         }
+        headingText.style.left = newLeftPos + "vw";
     }
+}
 
 
-})
+)
